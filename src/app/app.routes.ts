a0738919f5f39dbe1527c7532cb2c00760fd0e2a
ankita <ankitabasu.8420539497@gmail.com>
2024-11-21 21:17:41 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default redirection
+    { path: '**', redirectTo: '/home' }, // Wildcard route for 404
     {path:'search',component:SearchComponent},
     {path:'home',component:HomeComponent},
     { path: 'details/:show_id', component: DetailspageComponent },
