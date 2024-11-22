2950dab05f868a7aa5c197b5d481ed6a94063b7c
ankita <ankitabasu.8420539497@gmail.com>
2024-11-22 21:26:23 +0530

    Update auth.interceptor.ts

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,22 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  // Get the token from localStorage
-  const token = localStorage.getItem('token');
+ // Get the token from localStorage
+ const token = localStorage.getItem('token');
 
-  // Clone the request to add the Authorization header if the token exists
-  let clonedReq = req;
-  if (token) {
-    clonedReq = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  }
+ // Clone the request to add the Authorization header if the token exists
+ if (token) {
+   const clonedReq = req.clone({
+     setHeaders: {
+       Authorization: `Bearer ${token}`,
+     },
+   });
+   return next(clonedReq);
+ }
 
-  // Add withCredentials: true to the cloned request to send credentials with the request
-  clonedReq = clonedReq.clone({ withCredentials: true });
+ // Pass the original request if no token is found
+ return next(req);
+}
 
-  // Pass the modified request
-  return next(clonedReq);
-};
